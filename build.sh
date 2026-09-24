52e9e0be6a5938cf080e1794ceec1bdcc4c4bc84
ashishfogawat3208-oss <ashishfogawat3208@gmail.com>
2026-09-24 12:37:35 +0530

    Run migrations during Render build

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip
-
-python -m pip install -r requirements.txt
-
+python manage.py migrate --noinput
 python manage.py collectstatic --no-input
